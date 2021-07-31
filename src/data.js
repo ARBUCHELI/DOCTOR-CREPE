cca8ee0538cbaa6addc027184b743e862afd2e3a
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-07-31 17:48:42 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const allRecipesData = [
+    { id: 0, name: 'Chocolate & Mantequilla', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/hankmacmuffin.jpg'},
+    { id: 1, name: 'Arandanos & Crema', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/berrymanilow.jpg'},
+    { id: 2, name: 'Trocitos de Banana Split', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/bananasplit.jpg'},
+    { id: 3, name: 'Cafe Vainilla & Caramelo', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/frapuccino.jpg'},
+    { id: 4, name: 'Tortilla Española', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/tortillaespanola.jpg'},
+    { id: 5, name: 'Delicia de Chocolate', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/millyvanilly.jpg'},
+    { id: 6, name: 'Nutella & Crema', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/thejigglyfluff.jpg'},
+    { id: 7, name: 'Sundae con Cereza', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/cherrysundae.jpg'},
+    { id: 8, name: 'Te Helado Francés', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/teheladofrances.jpg'},
+    { id: 9, name: 'Pizza Chorizo Ibérico', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/pizzaiberica.jpg'},
+    { id: 10, name: 'Muffin Banano & Avena', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/bananoyavena.jpg'},
+    { id: 11, name: 'Crepe Jamón & Queso', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/jamonyqueso.jpg'},
+    { id: 12, name: 'Helado Vainilla & Oreos', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/heladovainillayoreo.jpg'},
+    { id: 13, name: 'Malteada de Fresa & Crema', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/malteadadefresa.jpg'},
+    { id: 14, name: 'Bocata de Jamón Serrano', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/bocatadejamonserrano.jpg'},
+    { id: 15, name: 'Manzana & Coco Tostado', img: 'https://raw.githubusercontent.com/ARBUCHELI/DOCTOR-CREPE/master/images/Muffins-de-coco-y-manzana.jpg'},
+  ];
+  
+  export default allRecipesData;
